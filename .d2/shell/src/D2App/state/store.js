dea8ebd795ebf7cd6af0a5391c4ac17101578487
manzzup <manujith.nc@gmail.com>
2020-03-24 21:20:03 +0530

    Updates

--- .d2/shell/src/D2App/state/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore, combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
-import appReducer from './appState';
-export const rootReducer = combineReducers({
-  app: appReducer
-});
-export const store = configureStore({
-  reducer: rootReducer
-});
